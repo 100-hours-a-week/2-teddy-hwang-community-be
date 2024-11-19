cfbc8d593d68c53fca030aa7bfdffde1c2faf147
ghkddlscks19 <ghkddlscks19@naver.com>
2024-11-19 16:15:01 +0900

    fix: 상태 코드 값 안들어 가는거 수정

--- middleware/customError.js
+++ middleware/customError.js
@@ -1,21 +1,21 @@
-// class CustomError extends Error{
-//     constructor(message, statusCode) {
-//         super(message);
-//         this.name = this.constructor.name;
-//         this.statusCode;
-//     }
-// }
+class CustomError extends Error{
+    constructor(message, statusCode) {
+        super(message);
+        this.name = this.constructor.name;
+        this.statusCode = statusCode;
+    }
+}
 
-// class BadRequest extends CustomError{
-//     constructor(message = '잘못된 요청입니다') {
-//         super(message, 400);
-//     }
-// }
+class BadRequest extends CustomError{
+    constructor(message = '잘못된 요청입니다') {
+        super(message, 400);
+    }
+}
 
-// class InternalServerError extends CustomError{
-//     constructor(message = '서버에 문제가 발생했습니다') {
-//         super(message, 500);
-//     }
-// }
+class InternalServerError extends CustomError{
+    constructor(message = '서버에 문제가 발생했습니다') {
+        super(message, 500);
+    }
+}
 
-// module.exports = {CustomError, BadRequest, InternalServerError};
+module.exports = {CustomError, BadRequest, InternalServerError};
