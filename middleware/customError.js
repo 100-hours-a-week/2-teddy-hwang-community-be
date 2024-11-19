617ac26bbfdc04326086d66cb8def36ac11b491b
ghkddlscks19 <ghkddlscks19@naver.com>
2024-11-19 09:48:50 +0900

    feat: 에러 핸들링 추후 수정 예정

---
+++ middleware/customError.js
@@ -0,0 +1,21 @@
+// class CustomError extends Error{
+//     constructor(message, statusCode) {
+//         super(message);
+//         this.name = this.constructor.name;
+//         this.statusCode;
+//     }
+// }
+
+// class BadRequest extends CustomError{
+//     constructor(message = '잘못된 요청입니다') {
+//         super(message, 400);
+//     }
+// }
+
+// class InternalServerError extends CustomError{
+//     constructor(message = '서버에 문제가 발생했습니다') {
+//         super(message, 500);
+//     }
+// }
+
+// module.exports = {CustomError, BadRequest, InternalServerError};
