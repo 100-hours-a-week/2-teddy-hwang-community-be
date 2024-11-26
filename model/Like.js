46187e3080359dbf7774eaae5d20b0213c1b8b10
ghkddlscks19 <ghkddlscks19@naver.com>
2024-11-26 23:15:06 +0900

    fix: 좋아요 상태 확인 user_id 정수형으로 변경

--- model/Like.js
+++ model/Like.js
@@ -106,7 +106,7 @@ class Like {
         const likes = this.readLikesFile();
         
         // likes 배열에서 해당 게시글과 사용자에 대한 좋아요가 존재하는지 확인
-        const existLike = likes.find(like => like.post_id === Number(postId) && like.user_id === userId);
+        const existLike = likes.find(like => like.post_id === Number(postId) && like.user_id === Number(userId));
         
         return existLike !== undefined; // 좋아요가 존재하면 true, 없으면 false 반환
     }
