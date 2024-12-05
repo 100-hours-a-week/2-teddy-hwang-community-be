79b3161d1fa020a03d0f5e3490b0e408c5ba84eb
ghkddlscks19 <ghkddlscks19@naver.com>
2024-12-05 14:34:25 +0900

    userId 세션 적용 및 좋아요 응답 json에 좋아요 수 추가

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -1,42 +1,51 @@
 const { BadRequest, InternalServerError } = require('../middleware/customError');
 const LikeModel = require('../model/Like');
+const PostModel = require('../model/Post');
 
 //좋아요 추가
 const addLike = async (req, res, next) => {
     try {
         const postId = req.params.post_id;
-        const userId = req.body.user_id;
+        const userId = req.session.user.id;
 
         if(!userId || !postId) {
             next(new BadRequest());
         }
 
         const like = await LikeModel.addLike(postId, userId);
+        const likeCount = await PostModel.getLikeCount(postId);
 
         res.status(201).json({
             message: '좋아요 추가를 성공했습니다.',
-            post_id: like.post_id
+            data: {
+                post_id: like.post_id,
+                like_count: likeCount
+            }           
         })
         
     }catch(error) {
-        next(new InternalServerError());
+        return next(new InternalServerError());
     }
 }
 //좋아요 취소
 const removeLike = async (req, res, next) => {
     try {
         const postId = req.params.post_id;
-        const userId = req.query.userId;
+        const userId = req.session.user.id;
 
         if(!userId || !postId) {
             next(new BadRequest());
         }
 
         const like = await LikeModel.removeLike(postId, userId);
+        const likeCount = await PostModel.getLikeCount(postId);
 
         res.status(200).json({
             message: '좋아요 취소를 성공했습니다.',
-            post_id: like
+            data: {
+                post_id: like.post_id,
+                like_count: likeCount
+            }       
         })
         
     }catch(error) {
@@ -47,7 +56,7 @@ const removeLike = async (req, res, next) => {
 const isLikedByUser = async (req, res, next) => {
     try {
         const postId = req.params.post_id;
-        const userId = req.query.userId;
+        const userId = req.session.user.id;
 
         if(!userId || !postId) {
             next(new BadRequest());
