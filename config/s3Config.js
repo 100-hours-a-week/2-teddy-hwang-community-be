8b390978dee848e512ebde2360263ffe94557003
ghkddlscks19 <ghkddlscks19@naver.com>
2024-12-19 01:04:21 +0900

    fix: imageUpload를 게시글, 유저 분리

--- config/s3Config.js
+++ config/s3Config.js
@@ -13,24 +13,34 @@ const s3Client = new S3Client({
     } 
 });
 
-const storage = multerS3({
-    s3: s3Client,
-    bucket: process.env.S3_BUCKET_NAME,
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    key: (req, file, cb) => {
-        //원본 파일명 디코딩
-        const originalFilename = Buffer.from(file.originalname, 'latin1').toString('utf8');
-        //확장자 앞의 이름 가져오기 ex)image.png -> image 가져오기 
-        const lastDotIndex = originalFilename.lastIndexOf('.');
-        const filename = originalFilename.substring(0, lastDotIndex);
-        console.log(filename);
-        const fileExtension = file.originalname.split('.').pop();
-        cb(null, `posts/${Date.now()}-${uuid()}-${filename}.${fileExtension}`);
+const createUploader = (folder) => {
+    return multerS3({
+        s3: s3Client,
+        bucket: process.env.S3_BUCKET_NAME,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: (req, file, cb) => {
+            const originalFilename = Buffer.from(file.originalname, 'latin1').toString('utf8');
+            const lastDotIndex = originalFilename.lastIndexOf('.');
+            const filename = originalFilename.substring(0, lastDotIndex);
+            const fileExtension = file.originalname.split('.').pop();
+            cb(null, `${folder}/${Date.now()}-${uuid()}-${filename}.${fileExtension}`);
+        }
+    });
+};
+
+const postUpload = multer({
+    storage: createUploader('posts'),
+    limits: { fileSize: 5 * 1024 * 1024 },
+    fileFilter: (req, file, cb) => {
+        if (!file.mimetype.startsWith('image/')) {
+            return cb(new BadRequest('이미지 파일만 업로드 가능합니다.'), false);
+        }
+        cb(null, true);
     }
 });
 
-const upload = multer({
-    storage,
+const userUpload = multer({
+    storage: createUploader('users'),
     limits: { fileSize: 5 * 1024 * 1024 },
     fileFilter: (req, file, cb) => {
         if (!file.mimetype.startsWith('image/')) {
@@ -57,8 +67,9 @@ const deleteImage = async (fileKey) => {
     }
  };
 
-module.exports = {
+ module.exports = {
     s3Client,
-    upload,
+    postUpload,
+    userUpload,
     deleteImage
 };
