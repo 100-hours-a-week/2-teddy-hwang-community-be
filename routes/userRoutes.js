e4e4454e34a5a221578c3c174f0dc258d4445b90
ghkddlscks19 <ghkddlscks19@naver.com>
2024-12-27 21:32:36 +0900

    feat: user 기능 db 연결

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { isAuthenticated, isResourceOwner } = require('../middleware/auth');
-const { createUser, login, getUserDetails, updateUserInfo, updatePassword, existsByEmail, existsByNickname, checkPasswordMatch, deleteUser, existsByNicknameUpdate } = require('../controllers/userController');
+const {  createUser, login, getUserDetails, updateUserInfo, updateUserPassword, existsByEmail, checkNicknameSignup, checkNicknameUpdate, removeUser } = require('../controllers/userController');
 
 const router = express.Router();
 
@@ -9,14 +9,13 @@ router.post('/login', login);
 
 router.get('/:user_id', isAuthenticated, isResourceOwner, getUserDetails);
 router.get('/email/:email', existsByEmail);
-router.get('/signup/nickname/:nickname', existsByNickname);
-router.get('/profile/nickname/:nickname', existsByNicknameUpdate);
-router.get('/:user_id/:password', isAuthenticated, isResourceOwner, checkPasswordMatch);
+router.get('/signup/nickname/:nickname', checkNicknameSignup);
+router.get('/profile/nickname/:nickname', checkNicknameUpdate);
 
 router.patch('/:user_id/profile', ...updateUserInfo);
-router.patch('/:user_id/password', isAuthenticated, isResourceOwner, updatePassword);
+router.patch('/:user_id/password', isAuthenticated, isResourceOwner, updateUserPassword);
 
-router.delete('/:user_id', isAuthenticated, isResourceOwner, deleteUser);
+router.delete('/:user_id', isAuthenticated, isResourceOwner, removeUser);
 
 module.exports = router;
 
