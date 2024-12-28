44e30c6521ea8b098440a071f449b6339d57282d
ghkddlscks19 <ghkddlscks19@naver.com>
2024-12-28 10:21:03 +0900

    feat: MySQL  session으로 변경

--- app.js
+++ app.js
@@ -2,15 +2,30 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const fileStore = require('session-file-store')(session);
+const { sessionStore } = require('./config/dbConfig');
 require('dotenv').config();
-const app = express();
 
 const postRoutes = require('./routes/postRoutes');
 const userRoutes = require('./routes/userRoutes');
 const commentRoutes = require('./routes/commentRoutes');
 
+const app = express();
 
+// DB 연결
+const startServer = async () => {
+    try {
+        console.log('데이터베이스 연결 성공');
+
+        app.listen(process.env.PORT, () => {
+            console.log(`서버가 작동중입니다. http://localhost:${process.env.PORT}`);
+        });
+    } catch (err) {
+        console.error('데이터베이스 연결 실패:', err);
+        process.exit(1);
+    }
+};
+
+// cors 설정
 app.use(cors({
     origin: [
         process.env.CORS_ORIGIN,
@@ -19,12 +34,13 @@ app.use(cors({
     credentials: true
 }));
 
+// 쿠키 및 세션 설정
 app.use(cookieParser());
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: process.env.SESSION_RESAVE === 'true',
     saveUninitialized: process.env.SESSION_SAVE_UNINITIALIZED === 'true',
-    store: new fileStore(),
+    store: sessionStore,
     cookie: {
         httpOnly: process.env.SESSION_COOKIE_HTTP_ONLY === 'true',
         secure: process.env.SESSION_COOKIE_SECURE === 'true',
@@ -32,8 +48,10 @@ app.use(session({
     }
 }));
 
+// json 설정
 app.use(express.json());
 
+// 경로 설정
 app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/comments', commentRoutes);
@@ -46,7 +64,6 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`서버가 작동중입니다. http://localhost:${process.env.PORT}`);
-});
+startServer();
+
 
