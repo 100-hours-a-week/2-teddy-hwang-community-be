e871d7749f63c21dbedc03a4bef2c475966dd4f4
ghkddlscks19 <ghkddlscks19@naver.com>
2025-01-06 09:40:14 +0900

    feat: 댓글 기능 db 연결

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createComment, updateComment, findByUserId, deleteComment,  } = require('../controllers/commentController')
+const { createComment, updateComment, findCommentUser, deleteComment,  } = require('../controllers/commentController')
 
 const router = express.Router();
 
@@ -7,7 +7,7 @@ router.post('/', createComment);
 
 router.patch('/:comment_id', updateComment);
 
-router.get('/:comment_id', findByUserId);
+router.get('/:comment_id', findCommentUser);
 
 router.delete('/:comment_id', deleteComment);
 
