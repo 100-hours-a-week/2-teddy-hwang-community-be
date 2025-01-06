de733aa8426cbcdbbf64ed45d8d05403dc668295
ghkddlscks19 <ghkddlscks19@naver.com>
2025-01-06 09:52:01 +0900

    feat: 로그아웃 route 추가

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { isAuthenticated, isResourceOwner } = require('../middleware/auth');
-const {  createUser, login, getUserDetails, updateUserInfo, updateUserPassword, existsByEmail, checkNicknameSignup, checkNicknameUpdate, removeUser } = require('../controllers/userController');
+const {  createUser, login, getUserDetails, updateUserInfo, updateUserPassword, existsByEmail, checkNicknameSignup, checkNicknameUpdate, removeUser, logout } = require('../controllers/userController');
 
 const router = express.Router();
 
 router.post('/', ...createUser);
 router.post('/login', login);
+router.post('/logout', logout);
 
 router.get('/:user_id', isAuthenticated, isResourceOwner, getUserDetails);
 router.get('/email/:email', existsByEmail);
