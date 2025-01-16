e790a02d1190ebb239671d778228524fd0088f42
ghkddlscks19 <ghkddlscks19@naver.com>
2025-01-16 14:27:03 +0900

    feat: authMiddleware 추가

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,22 +1,31 @@
+// routes/userRoutes.js
 const express = require('express');
-const { isAuthenticated, isResourceOwner } = require('../middleware/auth');
-const {  createUser, login, getUserDetails, updateUserInfo, updateUserPassword, existsByEmail, checkNicknameSignup, checkNicknameUpdate, removeUser, logout } = require('../controllers/userController');
+const { 
+    createUser, 
+    getUserDetails, 
+    updateUserInfo, 
+    updateUserPassword, 
+    existsByEmail, 
+    checkNicknameSignup, 
+    checkNicknameUpdate, 
+    removeUser 
+} = require('../controllers/userController');
+const { authMiddleware } = require('../middleware/auth');
 
 const router = express.Router();
 
+// 회원가입
 router.post('/', ...createUser);
-router.post('/login', login);
-router.post('/logout', logout);
 
-router.get('/:user_id', isAuthenticated, isResourceOwner, getUserDetails);
+// 이메일, 닉네임 중복 체크
 router.get('/email/:email', existsByEmail);
 router.get('/signup/nickname/:nickname', checkNicknameSignup);
-router.get('/profile/nickname/:nickname', checkNicknameUpdate);
+router.get('/profile/nickname/:nickname', authMiddleware, checkNicknameUpdate);
 
-router.patch('/:user_id/profile', ...updateUserInfo);
-router.patch('/:user_id/password', isAuthenticated, isResourceOwner, updateUserPassword);
-
-router.delete('/:user_id', isAuthenticated, isResourceOwner, removeUser);
+// 유저 정보 관련
+router.get('/:user_id', authMiddleware, getUserDetails);
+router.patch('/:user_id/profile', authMiddleware, ...updateUserInfo);
+router.patch('/:user_id/password', authMiddleware, updateUserPassword);
+router.delete('/:user_id', authMiddleware, removeUser);
 
 module.exports = router;
-
