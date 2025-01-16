e798d8ffe08e639f0228e3d6f2bdd1c66826d3aa
ghkddlscks19 <ghkddlscks19@naver.com>
2025-01-16 14:24:48 +0900

    feat: authMiddleware 추가

--- routes/likeRoutes.js
+++ routes/likeRoutes.js
@@ -1,12 +1,16 @@
+// routes/likeRoutes.js
 const express = require('express');
-const { addLike, removeLike, isLikedByUser } = require('../controllers/likeController');
+const { 
+    addLike, 
+    removeLike, 
+    isLikedByUser 
+} = require('../controllers/likeController');
+const { authMiddleware } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.post('/:post_id/like', addLike);
-
-router.delete('/:post_id/like', removeLike);
-
-router.get('/:post_id/like', isLikedByUser);
+router.post('/:post_id/like', authMiddleware, addLike);
+router.delete('/:post_id/like', authMiddleware, removeLike);
+router.get('/:post_id/like', authMiddleware, isLikedByUser);
 
 module.exports = router;
