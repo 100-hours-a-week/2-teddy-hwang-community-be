d324e124532f0ae104878a7acf700169eefcebda
ghkddlscks19 <ghkddlscks19@naver.com>
2025-01-16 13:49:17 +0900

    feat: userId를 session -> jwt 방식으로 변경

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -6,7 +6,7 @@ const { findLikeCount } = require('../model/Post');
 const addLike = async (req, res, next) => {
     try {
         const postId = Number(req.params.post_id);
-        const userId = Number(req.session.user.id);
+        const userId = Number(req.user.id);
 
         if(!userId || !postId) {
             next(new BadRequest());
@@ -31,7 +31,7 @@ const addLike = async (req, res, next) => {
 const removeLike = async (req, res, next) => {
     try {
         const postId = Number(req.params.post_id);
-        const userId = Number(req.session.user.id);
+        const userId = Number(req.user.id);
 
         if(!userId || !postId) {
             next(new BadRequest());
@@ -56,7 +56,7 @@ const removeLike = async (req, res, next) => {
 const isLikedByUser = async (req, res, next) => {
     try {
         const postId = Number(req.params.post_id);
-        const userId = Number(req.session.user.id);
+        const userId = Number(req.user.id);
 
         if(!userId || !postId) {
             next(new BadRequest());
