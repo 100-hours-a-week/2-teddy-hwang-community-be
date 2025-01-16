6acd8922c08680daaf6779b520149e9a2532497a
ghkddlscks19 <ghkddlscks19@naver.com>
2025-01-16 13:46:45 +0900

    인증 기능 라우트 추가

---
+++ routes/authRoutes.js
@@ -0,0 +1,18 @@
+// routes/authRoutes.js
+const express = require('express');
+const { 
+    login, 
+    refresh, 
+    logout, 
+    logoutAll 
+} = require('../controllers/authController');
+const { authMiddleware, refreshTokenMiddleware } = require('../middleware/auth');
+
+const router = express.Router();
+
+router.post('/login', login);
+router.post('/logout', authMiddleware, logout);
+router.post('/logout-all', authMiddleware, logoutAll);
+router.post('/refresh', refreshTokenMiddleware, refresh);
+
+module.exports = router;
