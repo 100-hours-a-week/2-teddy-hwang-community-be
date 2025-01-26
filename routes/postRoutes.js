e2147b3995ae7969a2657f16eb63630a3faa3f3b
ghkddlscks19 <ghkddlscks19@naver.com>
2025-01-26 23:05:21 +0900

    merge feature-db

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -16,14 +16,10 @@ const router = express.Router();
 router.post('/', authMiddleware, ...createPost);
 router.patch('/:post_id', authMiddleware, ...updatePost);
 
-<<<<<<< HEAD
-router.get('/', authMiddleware, getAllPosts);
-router.get('/:post_id', authMiddleware, getOnePost);
-router.get('/:post_id/without-view', authMiddleware, getOnePostWithoutView);
-=======
+
 router.get('/', getAllPosts);
 router.get('/:post_id', checkViewCount, getOnePost);
->>>>>>> feature-db
+
 
 router.delete('/:post_id', authMiddleware, deletePost);
 
